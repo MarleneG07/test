98a6c893b613486d219366b7898e590138608ffb
MarleneG07 <93849014+MarleneG07@users.noreply.github.com>
2022-01-04 02:56:09 -0600

    Add files via upload
    DISEÑO BASE PARA UN FORMULARIO.

---
+++ arreglo.js
@@ -0,0 +1,20 @@
+ const arreglo = [{},{}];
+arreglo; 
+
+//---------------------------------------------
+const numeros = [1,2,4,5];
+numeros.push(6);
+numeros;
+
+numeros.splice(2,0,3);
+numeros;
+
+numeros.unshift(0);
+numeros;
+
+//---------------------------------------------
+const colores = ["azul","verde","amarillo","rojo","naranja"];
+const colObt = colores.find(elemento => elemento === "rojo");
+colObt;
+
+ 
